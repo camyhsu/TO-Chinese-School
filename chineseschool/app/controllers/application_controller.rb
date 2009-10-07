253c6b61f2e238ffae6e70041d73f7c75590757f
camyhsu <camyhsu@yahoo.com>
2009-10-07 00:08:40 -0700

    Make SSL required in production.

--- chineseschool/app/controllers/application_controller.rb
+++ chineseschool/app/controllers/application_controller.rb
@@ -5,8 +5,18 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-
+  before_filter :ssl_required
+  
 
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password
+
+
+  def ssl_required
+    if RAILS_ENV == 'production'
+      unless request.ssl?
+        redirect_to("https://www.to-cs.org" + request.request_uri) and return false
+      end
+    end
+  end
 end
