e9276f715414d037ce6f9b3403fc3e8dd48fb381
camyhsu <camyhsu@yahoo.com>
2009-10-18 20:38:28 -0700

    Add a generic layout.  Add asset host to production environment so that it would recognize the extra path element of chineseschool (which is due to the specific production setup for TOCS.)

--- chineseschool/config/environments/production.rb
+++ chineseschool/config/environments/production.rb
@@ -19,7 +19,8 @@ config.action_view.cache_template_loading            = true
 # config.cache_store = :mem_cache_store
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host = "http://assets.example.com"
+config.action_controller.asset_host = 'http://to-cs.org/chineseschool'
+
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
