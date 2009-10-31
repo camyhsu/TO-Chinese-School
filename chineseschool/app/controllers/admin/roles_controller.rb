98186a20d808284fd2a9c4c464c1b662a4c28e05
camyhsu <camyhsu@yahoo.com>
2009-10-31 16:54:29 -0700

    Implement saving of new role.

--- chineseschool/app/controllers/admin/roles_controller.rb
+++ chineseschool/app/controllers/admin/roles_controller.rb
@@ -1,5 +1,7 @@
 class Admin::RolesController < ApplicationController
 
+  verify :only => :create, :method => :post, :add_flash => { :notice => 'Illegal GET' }, :redirect_to => { :action => 'index' }
+
   def index
     @roles = Role.find(:all)
   end
@@ -8,4 +10,12 @@ class Admin::RolesController < ApplicationController
     @role = Role.new
   end
 
+  def create
+    @role = Role.new(params[:role])
+    if @role.save
+      redirect_to :action => 'index'
+    else
+      render :action => 'new'
+    end
+  end
 end
--- chineseschool/spec/controllers/admin/roles_controller_spec.rb
+++ chineseschool/spec/controllers/admin/roles_controller_spec.rb
@@ -33,7 +33,7 @@ describe Admin::RolesController, 'displaying index' do
 end
 
 
-describe Admin::RolesController, 'displaying create form' do
+describe Admin::RolesController, 'displaying create role form' do
   before(:each) do
     stub_check_authentication_and_authorization_in @controller
     @new_role_created = Role.new
@@ -57,5 +57,58 @@ describe Admin::RolesController, 'displaying create form' do
 
   it 'should respond to correct route' do
     { :get => '/chineseschool/admin/roles/new' }.should route_to( :controller => 'admin/roles', :action => 'new' )
+  end
+end
+
+
+describe Admin::RolesController, 'creating role successfully' do
+  before(:each) do
+    stub_check_authentication_and_authorization_in @controller
+    new_role_name = random_string 8
+    @role_parameters = { 'name' => new_role_name }
+  end
+  
+  it 'should save a new role' do
+    fake_new_role = Role.new
+    Role.expects(:new).with(@role_parameters).once.returns(fake_new_role)
+    fake_new_role.expects(:save).once
+    post :create, :role => @role_parameters
+  end
+
+  it 'should redirect to index view' do
+    post :create, :role => @role_parameters
+    response.should redirect_to(:action => 'index')
+  end
+
+  it 'should respond to correct route' do
+    { :post => '/chineseschool/admin/roles/create' }.should route_to( :controller => 'admin/roles', :action => 'create' )
+  end
+  
+  it 'should redirect to index view if accessed using GET method' do
+    get :create
+    response.should redirect_to(:action => 'index')
+    flash[:notice].should == 'Illegal GET'
+  end
+end
+
+
+describe Admin::RolesController, 'failing to create role, tested with views' do
+  integrate_views
+  fixtures :roles
+  
+  before(:each) do
+    stub_check_authentication_and_authorization_in @controller
+  end
+
+  it 'should display error message in the form if role name is empty' do
+    post :create, :role => {}
+    response.should render_template(:new)
+    response.should include_text("Name can't be blank")
+  end
+
+  it 'should display error message in the form if role name is already taken' do
+    post :create, :role => { :name => 'Super User'}
+    response.should render_template(:new)
+    response.should include_text('Name has already been taken')
   end
 end
