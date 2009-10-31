2cc8de10d3d89dba1eaa53957f376420d42b290b
camyhsu <camyhsu@yahoo.com>
2009-10-31 13:29:45 -0700

    Change form layout for create new role to the admin form like sign in.

--- chineseschool/spec/views/admin/roles/new.html.erb_spec.rb
+++ chineseschool/spec/views/admin/roles/new.html.erb_spec.rb
@@ -11,25 +11,29 @@ describe '/admin/roles/new' do
     assigns[:title].should == 'Create Role'
   end
 
-  it 'should have a title in h1' do
-    response.should have_tag('h1', 1)
-    response.should have_tag('h1', 'Create Role')
+  it 'should have a form legend' do
+    response.should have_tag('legend', 1)
+    response.should have_tag('legend', 'Create Role')
   end
-
+  
   it 'should have correct form action' do
     response.should have_tag('form[action="/chineseschool/admin/roles/create"]', 1)
   end
 
+  it 'should have 2 div sections insdie the form' do
+    response.should have_tag('form div', 2)
+  end
+
   it 'should have the name field' do
-    response.should have_tag('form p') do |ps|
-      with_tag(ps[0], 'label', 'Name')
-      with_tag(ps[0], 'input#role_name[name^="role[name"][type="text"]', 1)
+    response.should have_tag('form div') do |divs|
+      with_tag(divs[0], 'label', 'Name')
+      with_tag(divs[0], 'input#role_name[name^="role[name"][type="text"]', 1)
     end
   end
 
   it 'should have the submit button' do
     response.should have_tag('form div') do |divs|
-      with_tag(divs[0], 'input[value="Create Role"][type="submit"]', 1)
+      with_tag(divs[1], 'input[value="Save New Role"][type="submit"]', 1)
     end
   end
 end
