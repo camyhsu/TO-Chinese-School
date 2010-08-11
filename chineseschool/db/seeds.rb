d7afed0a0aa5ed68052b94cc924b4fe5b7401af2
camyhsu <camyhsu@yahoo.com>
2010-08-10 23:39:07 -0700

    Add roles and rights seed data

--- chineseschool/db/seeds.rb
+++ chineseschool/db/seeds.rb
@@ -1,7 +1,36 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+ROLE_NAME_REGISTRATION_OFFICER = 'Registration Officer'
+ROLE_NAME_INSTRUCTOR = 'Instructor'
+
+
+#
+# Utility methods
+#
+def create_role(role_name)
+  role_created = Role.create(:name => role_name) if !Role.exists?(:name => role_name)
+  # Cleared out existing right associations if the role is not created new
+  Role.find_by_name(role_name).rights.clear if role_created.nil?
+end
+
+
+#
+# Create roles
+#
+create_role(ROLE_NAME_REGISTRATION_OFFICER)
+create_role(ROLE_NAME_INSTRUCTOR)
+
+#
+# Create rights
+#
+Right.delete_all
+list_school_classes = Right.create(:name => 'List School Classes', :controller => 'admin/school_classes', :action => 'index')
+list_grades = Right.create(:name => 'List Grades', :controller => 'admin/grades', :action => 'index')
+
+#
+# Assign rights to Registration Officer
+#
+registration_officer = Role.find_by_name(ROLE_NAME_REGISTRATION_OFFICER)
+registration_officer.rights << list_school_classes
+registration_officer.rights << list_grades
