7195237c5d99b66f9e40453592a219b3141d64b8
camyhsu <camyhsu@yahoo.com>
2010-09-30 21:41:04 -0700

    Add proper phone number formatting

--- chineseschool/app/models/address.rb
+++ chineseschool/app/models/address.rb
@@ -7,12 +7,20 @@ class Address < ActiveRecord::Base
   validates_numericality_of :cell_phone, :only_integer => true, :allow_blank => true
 
 
+  def home_phone
+    format_phone_number(read_attribute(:home_phone))
+  end
+
   def home_phone=(phone)
-    write_attribute(:home_phone, clean_number(phone))
+    write_attribute(:home_phone, clean_phone_number(phone))
+  end
+
+  def cell_phone
+    format_phone_number(read_attribute(:cell_phone))
   end
 
   def cell_phone=(phone)
-    write_attribute(:cell_phone, clean_number(phone))
+    write_attribute(:cell_phone, clean_phone_number(phone))
   end
 
 
@@ -21,12 +29,16 @@ class Address < ActiveRecord::Base
   end
   
   def email_and_phone_number_correct?(email_to_check, phone_number_to_check)
-    cleaned_phone_number = clean_number phone_number_to_check
+    cleaned_phone_number = clean_phone_number phone_number_to_check
     puts "Cleaning phone number #{phone_number_to_check} => #{cleaned_phone_number}"
     (self.email == email_to_check) and ((self.home_phone == cleaned_phone_number) or (self.cell_phone == cleaned_phone_number))
   end
 
-  def clean_number(phone_number_to_clean)
+  def format_phone_number(phone_number)
+    "(#{phone_number[0..2]}) #{phone_number[3..5]}-#{phone_number[6..-1]}"
+  end
+
+  def clean_phone_number(phone_number_to_clean)
     phone_number_to_clean.gsub /\D/, ''
   end
 end
