3354f5f0396b6d346650c3ad467d2350db4965e7
camyhsu <camyhsu@yahoo.com>
2010-10-02 09:59:24 -0700

    Implement school class column on elective class listing

--- chineseschool/app/models/school_class.rb
+++ chineseschool/app/models/school_class.rb
@@ -2,20 +2,35 @@ class SchoolClass < ActiveRecord::Base
   
   belongs_to :grade
 
-  # next two lines are used for regular class assignments, not elective class assignments
-  has_many :student_class_assignments
-  has_many :students, :through => :student_class_assignments
-
   validates_presence_of :english_name, :chinese_name
   validates_uniqueness_of :english_name, :chinese_name
 
   validates_numericality_of :max_size, :only_integer => true, :greater_than_or_equal_to => 0, :allow_nil => true
   validates_numericality_of :min_age, :only_integer => true, :greater_than_or_equal_to => 0, :allow_nil => true
   validates_numericality_of :max_age, :only_integer => true, :greater_than_or_equal_to => 0, :allow_nil => true
-
-
+  
+  
   def name
     "#{chinese_name}(#{english_name})"
+  end
+
+  def elective?
+    self.grade.nil?
+  end
+  
+  def student_class_assignments
+    return StudentClassAssignment.find_by_elective_class_id(self.id) if elective?
+    StudentClassAssignment.find_by_school_class_id(self.id)
+  end
+
+  def students
+    association_key = 'school_class_id'
+    association_key = 'elective_class_id' if elective?
+    people = Person.all :select => 'people.*', 
+        :from => 'people, student_class_assignments',
+        :conditions => ["people.id = student_class_assignments.student_id and student_class_assignments.#{association_key} = ?", self.id]
+    puts people.inspect
+    people
   end
   
   def instructor_assignment_history
