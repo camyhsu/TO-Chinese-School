9304227a73cc27c54a9447490db0c1e0ba34936f
camyhsu <camyhsu@yahoo.com>
2010-11-20 10:39:55 -0800

    Add test coverage for InstructorAssignment

--- chineseschool/app/models/instructor_assignment.rb
+++ chineseschool/app/models/instructor_assignment.rb
@@ -17,23 +17,25 @@ class InstructorAssignment < ActiveRecord::Base
 
 
   def start_date_string
-    self.start_date.try(:to_date)
+    self.start_date.to_s
   end
 
   def end_date_string
-    self.end_date.try(:to_date)
+    self.end_date.to_s
   end
 
   
   private
 
   def start_date_no_later_than_end_date
+    return if self.start_date.nil? or self.end_date.nil?
     if self.start_date > self.end_date
       errors.add_to_base('Start Date can not be later than End Date')
     end
   end
 
   def no_overlapping
+    return if self.school_year.nil? or self.school_class.nil? or self.start_date.nil?
     existing_assignments = InstructorAssignment.find_all_by_school_year_id_and_school_class_id_and_role(self.school_year.id, self.school_class.id, self.role)
     existing_assignments.each do |existing_assignment|
       if duration_overlap? existing_assignment
--- chineseschool/spec/models/instructor_assignment_spec.rb
+++ chineseschool/spec/models/instructor_assignment_spec.rb
@@ -1,6 +1,153 @@
 require 'spec_helper'
 
 describe InstructorAssignment do
+  fixtures :instructor_assignments, :people, :school_classes, :school_years
 
-  it "should create a new instance given valid attributes"
+  before(:each) do
+    @instructor_assignment = instructor_assignments(:minimal)
+  end
+
+  it 'should define role constants' do
+    InstructorAssignment::ROLE_PRIMARY_INSTRUCTOR.should == 'Primary Instructor'
+    InstructorAssignment::ROLE_ROOM_PARENT.should == 'Room Parent'
+    InstructorAssignment::ROLE_SECONDARY_INSTRUCTOR.should == 'Secondary Instructor'
+    InstructorAssignment::ROLE_TEACHING_ASSISTANT.should == 'Teaching Assistant'
+
+    InstructorAssignment::ROLES.should include(InstructorAssignment::ROLE_PRIMARY_INSTRUCTOR)
+    InstructorAssignment::ROLES.should include(InstructorAssignment::ROLE_ROOM_PARENT)
+    InstructorAssignment::ROLES.should include(InstructorAssignment::ROLE_SECONDARY_INSTRUCTOR)
+    InstructorAssignment::ROLES.should include(InstructorAssignment::ROLE_TEACHING_ASSISTANT)
+  end
+
+  it 'should belongs to shool year' do
+    @instructor_assignment.school_year.should == school_years(:two_thousand_eight)
+  end
+
+  it 'should belongs to shool class' do
+    @instructor_assignment.school_class.should == school_classes(:first_grade)
+  end
+
+  it 'should belongs to instructor' do
+    @instructor_assignment.instructor.should == people(:person_one)
+  end
+  
+  it 'should be invalid without school year' do
+    @instructor_assignment.school_year = nil
+    @instructor_assignment.should_not be_valid
+    @instructor_assignment.school_year = school_years(:two_thousand_eight)
+    @instructor_assignment.should be_valid
+  end
+
+  it 'should be invalid without school class' do
+    @instructor_assignment.school_class = nil
+    @instructor_assignment.should_not be_valid
+    @instructor_assignment.school_class = school_classes(:first_grade)
+    @instructor_assignment.should be_valid
+  end
+
+  it 'should be invalid without instructor' do
+    @instructor_assignment.instructor = nil
+    @instructor_assignment.should_not be_valid
+    @instructor_assignment.instructor = people(:person_one)
+    @instructor_assignment.should be_valid
+  end
+
+  it 'should be invalid without start date' do
+    @instructor_assignment.start_date = nil
+    @instructor_assignment.should_not be_valid
+    @instructor_assignment.start_date = Date.parse('2008-09-06')
+    @instructor_assignment.should be_valid
+  end
+
+  it 'should be invalid without end date' do
+    @instructor_assignment.end_date = nil
+    @instructor_assignment.should_not be_valid
+    @instructor_assignment.end_date = Date.parse('2009-06-06')
+    @instructor_assignment.should be_valid
+  end
+
+  it 'should be invalid without role' do
+    @instructor_assignment.role = nil
+    @instructor_assignment.should_not be_valid
+    @instructor_assignment.role = random_string 8
+    @instructor_assignment.should be_valid
+  end
+
+  it 'should be invalid if start date is later than end date' do
+    @instructor_assignment.start_date = Date.parse('2008-09-07')
+    @instructor_assignment.end_date = Date.parse('2008-09-06')
+    @instructor_assignment.should_not be_valid
+    @instructor_assignment.start_date = Date.parse('2008-09-06')
+    @instructor_assignment.should be_valid
+  end
+
+  it 'should be invalid if dates overlapping with another instructor assignment - starts before the other ends' do
+    @instructor_assignment.start_date = Date.parse('2008-09-07')
+    @instructor_assignment.end_date = Date.parse('2008-10-06')
+    setup_expectations_for_finding_existing_instructor_assignments
+    @instructor_assignment.should_not be_valid
+  end
+
+  it 'should be invalid if dates overlapping with another instructor assignment - ends after the other starts' do
+    @instructor_assignment.start_date = Date.parse('2008-06-07')
+    @instructor_assignment.end_date = Date.parse('2008-07-16')
+    setup_expectations_for_finding_existing_instructor_assignments
+    @instructor_assignment.should_not be_valid
+  end
+
+  it 'should be invalid if dates overlapping with another instructor assignment - enclosing dates for the other' do
+    @instructor_assignment.start_date = Date.parse('2008-07-07')
+    @instructor_assignment.end_date = Date.parse('2008-09-16')
+    setup_expectations_for_finding_existing_instructor_assignments
+    @instructor_assignment.should_not be_valid
+  end
+
+  it 'should be invalid if dates overlapping with another instructor assignment - enclosed by dates for the other' do
+    @instructor_assignment.start_date = Date.parse('2008-08-17')
+    @instructor_assignment.end_date = Date.parse('2008-08-18')
+    setup_expectations_for_finding_existing_instructor_assignments
+    @instructor_assignment.should_not be_valid
+  end
+
+  def setup_expectations_for_finding_existing_instructor_assignments
+    existing_instructor_assignment_one = InstructorAssignment.new
+    existing_instructor_assignment_one.start_date = Date.parse('2008-07-10')
+    existing_instructor_assignment_one.end_date = Date.parse('2008-08-09')
+    existing_instructor_assignment_one.instructor = people(:person_one)
+    existing_instructor_assignment_two = InstructorAssignment.new
+    existing_instructor_assignment_two.start_date = Date.parse('2008-08-10')
+    existing_instructor_assignment_two.end_date = Date.parse('2008-09-10')
+    existing_instructor_assignment_two.instructor = people(:person_two)
+    InstructorAssignment.expects(:find_all_by_school_year_id_and_school_class_id_and_role).with(
+        @instructor_assignment.school_year.id, @instructor_assignment.school_class.id, @instructor_assignment.role).once.returns(
+        [existing_instructor_assignment_one, existing_instructor_assignment_two])
+  end
+end
+
+describe InstructorAssignment, 'displaying date as string' do
+  fixtures :instructor_assignments
+  
+  before(:each) do
+    @instructor_assignment = InstructorAssignment.new
+  end
+  
+  it 'should display start date string as yyyy-MM-dd' do
+    @instructor_assignment.start_date = Date.parse('2008-07-10')
+    @instructor_assignment.start_date_string.should == '2008-07-10'
+  end
+
+  it 'should return start date string as empty string if start date is nil' do
+    @instructor_assignment.start_date.should be_nil
+    @instructor_assignment.start_date_string.should == ''
+  end
+
+  it 'should display end date string as yyyy-MM-dd' do
+    @instructor_assignment.end_date = Date.parse('2008-07-10')
+    @instructor_assignment.end_date_string.should == '2008-07-10'
+  end
+
+  it 'should return end date string as empty string if end date is nil' do
+    @instructor_assignment.end_date.should be_nil
+    @instructor_assignment.end_date_string.should == ''
+  end
 end
