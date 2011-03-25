3687ef277d242e747fed7641d08c943fd6a79858
camyhsu <camyhsu@yahoo.com>
2011-03-24 21:25:57 -0700

    Implement adding user (by id) to a role

--- chineseschool/app/controllers/admin/roles_controller.rb
+++ chineseschool/app/controllers/admin/roles_controller.rb
@@ -7,4 +7,18 @@ class Admin::RolesController < ApplicationController
   def show
     @role = Role.find_by_id params[:id].to_i
   end
+
+  def add_user
+    @role = Role.find_by_id params[:id].to_i
+    @role.users << User.find_by_id(params[:user_id].to_i)
+    @role.save!
+    redirect_to :action => :show, :id => @role.id
+  end
+
+  def remove_user
+    @role = Role.find_by_id params[:id].to_i
+    @role.users.delete User.find_by_id(params[:user_id].to_i)
+    @role.save!
+    redirect_to :action => :show, :id => @role.id
+  end
 end
