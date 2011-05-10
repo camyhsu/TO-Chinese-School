dc8ce31180d9bf4bb1001bb816768621d7331885
camyhsu <camyhsu@yahoo.com>
2011-05-09 21:15:20 -0700

    Fix ajax issues caused by upgrading Rails to 2.3.11

--- chineseschool/public/javascripts/application.js
+++ chineseschool/public/javascripts/application.js
@@ -1,20 +1,7 @@
-// The following adds CCRF authentication token to ajax post
-// This requires the setup of AUTH_TOKEN in the header section (in layout, see app/views/layouts/_html_head_section.html.erb)
-// This code is a combination of the great contributions from:
-// http://henrik.nyh.se/2008/05/rails-authenticity-token-with-jquery
-// http://pastie.org/212866
-// http://www.viget.com/extend/ie-jquery-rails-and-http-oh-my
-//
-
-(function($) {
-    $().ajaxSend(function(event, request, settings) {
-        if (settings.type == 'GET' || settings.type == 'get' || typeof(AUTH_TOKEN) == "undefined") return;
-        //request.setRequestHeader("Accept", "text/javascript, text/html, application/xml, text/xml, */*");
-        request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        settings.data = settings.data || "";
-        settings.data += ((settings.data == "") ? "" : "&") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
-    });
-})(jQuery);
+$(document).ajaxSend(function(e, xhr, options) {
+  var token = $("meta[name='csrf-token']").attr("content");
+  xhr.setRequestHeader("X-CSRF-Token", token);
+});
 
 
 $(document).ready(function() {
