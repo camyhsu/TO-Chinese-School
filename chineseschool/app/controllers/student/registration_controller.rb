1c5d1205d809b538d03cdaed20c625a89b590645
camyhsu <camyhsu@yahoo.com>
2011-05-15 00:07:25 -0700

    Implement displaying registration options - base setup

--- chineseschool/app/controllers/student/registration_controller.rb
+++ chineseschool/app/controllers/student/registration_controller.rb
@@ -3,10 +3,15 @@ class Student::RegistrationController < ApplicationController
   before_filter :action_authorized?
 
   def display_registration_options
-
+    registration_school_year = SchoolYear.find_by_id params[:id].to_i
+    @possible_students = []
+    @user.person.families.each do |family|
+      @possible_students += family.children
+    end
+    @available_elective_classes = SchoolClass.find_available_elective_classes_for_registration registration_school_year
   end
   
-  def select_class_assignments
+  def display_pricing
     
   end
 
