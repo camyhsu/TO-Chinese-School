6e7c4539c274144672d9d74b8372c1cd2ba1229f
camyhsu <camyhsu@yahoo.com>
2011-05-28 09:10:43 -0700

    Add tuition and fee calculation to StudentFeePayment

--- chineseschool/app/models/student_fee_payment.rb
+++ chineseschool/app/models/student_fee_payment.rb
@@ -8,5 +8,36 @@ class StudentFeePayment < ActiveRecord::Base
   validates_numericality_of :registration_fee_in_cents, :only_integer => true, :allow_nil => false
   validates_numericality_of :tuition_in_cents, :only_integer => true, :allow_nil => false
   validates_numericality_of :book_charge_in_cents, :only_integer => true, :allow_nil => false
-  
+
+
+  def fill_in_tuition_and_fee(school_year, grade, existing_student_count_in_family)
+    self.registration_fee_in_cents = school_year.registration_fee_in_cents
+    self.book_charge_in_cents = school_year.book_charge_in_cents
+    calculate_tuition school_year, grade, existing_student_count_in_family
+  end
+
+  def calculate_tuition(school_year, grade, existing_student_count_in_family)
+    if Date.today <= school_year.pre_registration_end_date
+      self.pre_registration = true
+      self.tuition_in_cents = school_year.pre_registration_tuition_in_cents
+    else
+      self.tuition_in_cents = school_year.tuition_in_cents
+    end
+    apply_pre_k_discount school_year, grade
+    apply_multiple_child_discount school_year, existing_student_count_in_family
+  end
+
+  def apply_pre_k_discount(school_year, grade)
+    if Grade::GRADE_PRESCHOOL == grade
+      self.pre_k_discount = true
+      self.tuition_in_cents -= school_year.tuition_discount_for_pre_k_in_cents
+    end
+  end
+
+  def apply_multiple_child_discount(school_year, existing_student_count_in_family)
+    if existing_student_count_in_family >= 2
+      self.multiple_child_discount = true
+      self.tuition_in_cents -= school_year.tuition_discount_for_three_or_more_child_in_cents
+    end
+  end
 end
---
+++ chineseschool/spec/models/student_fee_payment_spec.rb
@@ -0,0 +1,110 @@
+require 'spec_helper'
+
+describe StudentFeePayment, 'filling in tuition and fee' do
+  before(:each) do
+    @student_fee_payment = StudentFeePayment.new
+
+    @fake_school_year = SchoolYear.new
+    @registration_fee = rand 100000
+    @fake_school_year.registration_fee_in_cents = @registration_fee
+    @book_charge = rand 100000
+    @fake_school_year.book_charge_in_cents = @book_charge
+
+    @fake_grade = Grade.new
+    @fake_existing_student_count_in_family = rand 100
+  end
+  
+  it 'should fill in registration fee and book charge and calculate tuition' do
+    @student_fee_payment.registration_fee_in_cents.should be_nil
+    @student_fee_payment.book_charge_in_cents.should be_nil
+    @student_fee_payment.expects(:calculate_tuition).once.with(@fake_school_year, @fake_grade, @fake_existing_student_count_in_family)
+    @student_fee_payment.fill_in_tuition_and_fee(@fake_school_year, @fake_grade, @fake_existing_student_count_in_family)
+    @student_fee_payment.registration_fee_in_cents.should == @registration_fee
+    @student_fee_payment.book_charge_in_cents.should == @book_charge
+  end
+end
+
+describe StudentFeePayment, 'calculating tuition' do
+  before(:each) do
+    @student_fee_payment = StudentFeePayment.new
+
+    @fake_school_year = SchoolYear.new
+    @fake_school_year.pre_registration_end_date = Date.today
+    @tuition = rand 100000
+    @fake_school_year.tuition_in_cents = @tuition
+    @pre_registration_tuition = rand 100000
+    @fake_school_year.pre_registration_tuition_in_cents = @pre_registration_tuition
+
+    @fake_grade = Grade.new
+    @fake_existing_student_count_in_family = rand 100
+  end
+
+  it 'should apply PreK and multiple child discount' do
+    @student_fee_payment.expects(:apply_pre_k_discount).once.with(@fake_school_year, @fake_grade)
+    @student_fee_payment.expects(:apply_multiple_child_discount).once.with(@fake_school_year, @fake_existing_student_count_in_family)
+    @student_fee_payment.calculate_tuition(@fake_school_year, @fake_grade, @fake_existing_student_count_in_family)
+  end
+
+  it 'should use pre-registration tuition if today is on or before pre_registration_end_date' do
+    @student_fee_payment.expects(:apply_pre_k_discount).once.with(@fake_school_year, @fake_grade)
+    @student_fee_payment.expects(:apply_multiple_child_discount).once.with(@fake_school_year, @fake_existing_student_count_in_family)
+    @student_fee_payment.calculate_tuition(@fake_school_year, @fake_grade, @fake_existing_student_count_in_family)
+    @student_fee_payment.tuition_in_cents.should == @pre_registration_tuition
+  end
+
+  it 'should use pre-registration tuition if today is after pre_registration_end_date' do
+    @fake_school_year.pre_registration_end_date = Date.yesterday
+    @student_fee_payment.expects(:apply_pre_k_discount).once.with(@fake_school_year, @fake_grade)
+    @student_fee_payment.expects(:apply_multiple_child_discount).once.with(@fake_school_year, @fake_existing_student_count_in_family)
+    @student_fee_payment.calculate_tuition(@fake_school_year, @fake_grade, @fake_existing_student_count_in_family)
+    @student_fee_payment.tuition_in_cents.should == @tuition
+  end
+end
+
+describe StudentFeePayment, 'applying PreK discount' do
+  before(:each) do
+    @student_fee_payment = StudentFeePayment.new
+    @original_tuition = rand 100000
+    @student_fee_payment.tuition_in_cents = @original_tuition
+
+    @fake_school_year = SchoolYear.new
+    @discount = rand 1000
+    @fake_school_year.tuition_discount_for_pre_k_in_cents = @discount
+  end
+
+  it 'should apply PreK discount if given grade is PreK' do
+    @student_fee_payment.apply_pre_k_discount(@fake_school_year, Grade::GRADE_PRESCHOOL)
+    @student_fee_payment.pre_k_discount.should be_true
+    @student_fee_payment.tuition_in_cents.should == (@original_tuition - @discount)
+  end
+
+  it 'should not apply PreK discount if given grade is not PreK' do
+    @student_fee_payment.apply_pre_k_discount(@fake_school_year, Grade.new)
+    @student_fee_payment.pre_k_discount.should be_false
+    @student_fee_payment.tuition_in_cents.should == @original_tuition
+  end
+end
+
+describe StudentFeePayment, 'applying multiple child discount' do
+  before(:each) do
+    @student_fee_payment = StudentFeePayment.new
+    @original_tuition = rand 100000
+    @student_fee_payment.tuition_in_cents = @original_tuition
+
+    @fake_school_year = SchoolYear.new
+    @discount = rand 1000
+    @fake_school_year.tuition_discount_for_three_or_more_child_in_cents = @discount
+  end
+
+  it 'should apply multiple child discount if existing student count is 2 or more' do
+    @student_fee_payment.apply_multiple_child_discount(@fake_school_year, 2)
+    @student_fee_payment.multiple_child_discount.should be_true
+    @student_fee_payment.tuition_in_cents.should == (@original_tuition - @discount)
+  end
+
+  it 'should not apply multiple child discount if existing student count is 1 or less' do
+    @student_fee_payment.apply_multiple_child_discount(@fake_school_year, 1)
+    @student_fee_payment.multiple_child_discount.should be_false
+    @student_fee_payment.tuition_in_cents.should == @original_tuition
+  end
+end
