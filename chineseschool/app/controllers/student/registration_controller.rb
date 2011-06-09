86ed65f2c270a8abbd3769e1976ed22283065214
camyhsu <camyhsu@yahoo.com>
2011-06-08 23:42:12 -0700

    Check authorization when showing payment confirmation and transaction detail

--- chineseschool/app/controllers/student/registration_controller.rb
+++ chineseschool/app/controllers/student/registration_controller.rb
@@ -80,6 +80,10 @@ class Student::RegistrationController < ApplicationController
 
   def payment_confirmation
     @registration_payment = RegistrationPayment.find_by_id params[:id].to_i
+    unless @registration_payment.paid_by == @user.person
+      flash[:notice] = 'Access to requested payment confirmation not authorized'
+      redirect_to(:controller => '/home', :action => 'index') and return
+    end
     @gateway_transaction = @registration_payment.find_first_approved_gateway_transaction
   end
 
--- chineseschool/app/controllers/student/transaction_history_controller.rb
+++ chineseschool/app/controllers/student/transaction_history_controller.rb
@@ -6,6 +6,10 @@ class Student::TransactionHistoryController < ApplicationController
 
   def show
     @registration_payment = RegistrationPayment.find_by_id params[:id].to_i
+    unless @registration_payment.paid_by == @user.person
+      flash[:notice] = 'Access to requested payment confirmation not authorized'
+      redirect_to(:controller => '/home', :action => 'index') and return
+    end
     @gateway_transaction = @registration_payment.find_first_approved_gateway_transaction
   end
 end
