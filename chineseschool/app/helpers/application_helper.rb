f7eae09b281077b6925ed9c73bd1c89f40fbba6c
camyhsu <camyhsu@yahoo.com>
2011-06-08 19:06:47 -0700

    Change time display to Pacific timezone for payment confirmation

--- chineseschool/app/helpers/application_helper.rb
+++ chineseschool/app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     return 'Yes' if flag
     'No'
   end
+
+  def display_utc_time_in_pacific(utc_time)
+    utc_time.in_time_zone('Pacific Time (US & Canada)').to_formatted_s(:long)
+  end
 end
