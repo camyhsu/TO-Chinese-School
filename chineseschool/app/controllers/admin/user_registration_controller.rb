1d40ca4a3b1a0fef55e0f15aec47099c9fb7b2aa
camyhsu <camyhsu@yahoo.com>
2011-06-11 18:44:05 -0700

    Refactor single person invitation

--- chineseschool/app/controllers/admin/user_registration_controller.rb
+++ chineseschool/app/controllers/admin/user_registration_controller.rb
@@ -6,8 +6,7 @@ class Admin::UserRegistrationController < ApplicationController
       person = Person.find_by_id person_id
       flash.now[:notice] = "Person with id => #{person_id} not found" and return if person.nil?
       flash.now[:notice] = "Person with id => #{person_id} already has an account" and return unless person.user.nil?
-      email = SigninMailer.create_registration_invitation person, params[:email_destination]
-      SigninMailer.deliver email
+      send_invitation_to person, params[:email_destination]
       flash.now[:notice] = "Invitation sent to Person id => #{person_id}, name => #{person.name}, " +
           "email_destination => #{params[:email_destination]}"
     end
