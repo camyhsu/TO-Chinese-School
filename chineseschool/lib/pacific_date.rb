ce0b56a9d9d59fd97ba88170f1261e5bf34cdf01
camyhsu <camyhsu@yahoo.com>
2011-09-25 09:41:57 -0700

    Correct date boundary calculation to account for server not running in Pacific Time

--- chineseschool/lib/pacific_date.rb
+++ chineseschool/lib/pacific_date.rb
@@ -1,7 +1,9 @@
 class PacificDate
   
   def self.start_time_utc_for(date)
-    date.to_time.utc
+    server_local_start_time = date.to_time
+    sample_pacific_time = server_local_start_time.in_time_zone('Pacific Time (US & Canada)')
+    Time.at(server_local_start_time + server_local_start_time.utc_offset - sample_pacific_time.utc_offset).utc
   end
   
   def self.today
