5eaa1df9fd9eb7dbc362ad78a4d3945bcae61056
camyhsu <camyhsu@yahoo.com>
2013-10-18 19:23:55 -0700

    Add the generated skeleton for Rails 3 version

---
+++ chineseschool/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chineseschool::Application.config.secret_token = '51057827f7f0c993ff5948fefaf00621bf1b32b67d2c1c7c4ccb2e618c7b4f75583040e6aea1577ac9936d43982b301c3aac3296ec9278a3fd52cfa4131efb45'
