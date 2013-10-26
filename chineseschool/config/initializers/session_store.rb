1472b1b8a7a49358f3fbbff9d61ba99412baa718
camyhsu <camyhsu@yahoo.com>
2013-10-26 10:36:25 -0700

    Add more security settings to cookie sessions

--- chineseschool/config/initializers/session_store.rb
+++ chineseschool/config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Chineseschool::Application.config.session_store :cookie_store, key: '_chineseschool_session'
+Chineseschool::Application.config.session_store :cookie_store,
+                                                key: '_chineseschool_session',
+                                                secure: Rails.env.production?,
+                                                httponly: true,
+                                                expire_after: 60.minutes
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
