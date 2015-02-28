0665eb42dbddceb9bb54aa960b222161a2c8dcf3
camyhsu <camyhsu@yahoo.com>
2015-02-27 18:05:34 -0800

    Implement team member count in team assignment page

--- chineseschool/app/models/track_event_team.rb
+++ chineseschool/app/models/track_event_team.rb
@@ -2,6 +2,7 @@ class TrackEventTeam < ActiveRecord::Base
   attr_accessible :name
 
   belongs_to :track_event_program
+  has_many :track_event_signups
 
   validates :name, :track_event_program, presence: true
 
