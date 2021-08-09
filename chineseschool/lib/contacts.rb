5c1432ddec3959e4fa4526f046a21450eb62c613
Chiaming Hsu <camyhsu21@gmail.com>
2021-08-09 12:20:49 -0700

    Change accounting contact to treasurer@to-cs.org

--- chineseschool/lib/contacts.rb
+++ chineseschool/lib/contacts.rb
@@ -2,6 +2,6 @@ module Contacts
   REGISTRATION_CONTACT = 'registration@to-cs.org'
   TEXT_BOOK_MANAGER = 'academic-book@to-cs.org'
   WEB_SITE_SUPPORT = 'engineering@to-cs.org'
-  ACCOUNTING_CONTACT = 'accountant@to-cs.org'
+  ACCOUNTING_CONTACT = 'treasurer@to-cs.org'
   PVA_CONTACT = 'pva@to-cs.org'
 end
